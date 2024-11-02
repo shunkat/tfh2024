37f54fc493ffcd08499d357791b7e9e29d005498
Yusei Hiratsuka <1a2a3a4yyy@gmail.com>
2024-11-03 01:01:46 +0900

    firestore追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:tfh2024/presentation/display/diplay_pdf_view.dart';
 import 'package:tfh2024/presentation/upload/upload_pdf_view.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
