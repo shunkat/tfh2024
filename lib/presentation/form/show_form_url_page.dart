0f30f30301d99e72f22e7d1d1cefa33e57fa808a
shunkat <shunkat@icloud.com>
2024-11-03 04:23:14 +0900

    urlの表示とqrコードの表示を担当するviewを作成

---
+++ lib/presentation/form/show_form_url_page.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+import 'dart:typed_data' show Uint8List;
+import 'package:qr_flutter/qr_flutter.dart';
+import 'package:tfh2024/presentation/display/diplay_pdf_view.dart';
+import 'package:uuid/uuid.dart';
+import 'package:flutter/services.dart'; // Clipboard機能のために追加
+
+import '../upload/upload_pdf_view.dart';
+
+class ShowFormUrlPage extends StatefulWidget {
+  final PdfViewerArguments arguments;
+
+  const ShowFormUrlPage({
+    Key? key,
+    required this.arguments,
+  }) : super(key: key);
+
+  @override
+  State<ShowFormUrlPage> createState() => _ShowFormUrlPageState();
+}
+
+class _ShowFormUrlPageState extends State<ShowFormUrlPage> {
+  late String pdfId;
+  late String url;
+
+  @override
+  void initState() {
+    super.initState();
+    pdfId = _generateRandomId();
+    url = 'http://hogehoge/?pdfid=$pdfId';
+  }
+
+  String _generateRandomId() {
+    // ランダムな値でpdfIdを生成
+    return const Uuid().v4();
+  }
+
+  void _startPresentation() {
+    // pdfIdを引数に追加してPdfViewerScreenを呼び出し
+    final updatedArguments = widget.arguments.copyWith(pdfId: pdfId);
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => PDFViewerScreen(arguments: updatedArguments),
+      ),
+    );
+  }
+
+  void _copyToClipboard() {
+    Clipboard.setData(ClipboardData(text: url));
+    ScaffoldMessenger.of(context).showSnackBar(
+      const SnackBar(content: Text('URLをコピーしました')),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('プレゼンテーション設定'),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            mainAxisSize: MainAxisSize.min, // 中央に配置するために追加
+            children: [
+              Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(
+                    'URL: $url',
+                    style: const TextStyle(fontSize: 16),
+                  ),
+                  const SizedBox(width: 8),
+                  IconButton(
+                    icon: const Icon(Icons.copy),
+                    onPressed: _copyToClipboard,
+                  ),
+                ],
+              ),
+              const SizedBox(height: 20),
+              // QRコードを表示
+              QrImageView(
+                data: url,
+                version: QrVersions.auto,
+                size: 200.0,
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: _startPresentation,
+                child: const Text('発表開始'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
