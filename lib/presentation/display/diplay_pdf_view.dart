12c54b52b0f34f43f2155d635be3e5a8f68e23c8
Yusei Hiratsuka <1a2a3a4yyy@gmail.com>
2024-11-02 18:10:59 +0900

    pdfのページをボタン送りに

--- lib/presentation/display/diplay_pdf_view.dart
+++ lib/presentation/display/diplay_pdf_view.dart
@@ -2,29 +2,143 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:printing/printing.dart';
 
-class PDFViewerScreen extends StatelessWidget {
+class PDFViewerScreen extends StatefulWidget {
   const PDFViewerScreen({super.key});
+
+  @override
+  State<PDFViewerScreen> createState() => _PDFViewerScreenState();
+}
+
+class _PDFViewerScreenState extends State<PDFViewerScreen> {
+  late PageController _pageController;
+  int _currentPage = 0;
+  int _totalPages = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController();
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
 
   Future<Uint8List> _loadPdf() async {
     final ByteData data = await rootBundle.load('assets/sample.pdf');
     return data.buffer.asUint8List();
   }
 
+  void _previousPage() {
+    if (_currentPage > 0) {
+      _pageController.previousPage(
+        duration: const Duration(milliseconds: 300),
+        curve: Curves.easeInOut,
+      );
+    }
+  }
+
+  void _nextPage() {
+    if (_currentPage < _totalPages - 1) {
+      _pageController.nextPage(
+        duration: const Duration(milliseconds: 300),
+        curve: Curves.easeInOut,
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('PDF Viewer'),
+      body: PdfPreview.builder(
+        build: (format) => _loadPdf(),
+        pagesBuilder: (context, pages) {
+          _totalPages = pages.length;
+          return Stack(
+            children: [
+              PageView.builder(
+                controller: _pageController,
+                onPageChanged: (int page) {
+                  setState(() {
+                    _currentPage = page;
+                  });
+                },
+                itemCount: pages.length,
+                itemBuilder: (context, index) {
+                  return Container(
+                    margin: const EdgeInsets.all(16),
+                    padding: const EdgeInsets.all(16),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.5),
+                          spreadRadius: 2,
+                          blurRadius: 5,
+                          offset: const Offset(0, 3),
+                        ),
+                      ],
+                    ),
+                    child: Image(image: pages[index].image),
+                  );
+                },
+              ),
+
+              Text(
+                'AAAAAAAAAAAA',
+                style: TextStyle(fontSize: 30, color: Colors.black),
+              ),
+              // Navigation Buttons
+              Positioned(
+                left: 0,
+                right: 0,
+                bottom: 20,
+                child: Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      FloatingActionButton(
+                        onPressed: _currentPage > 0 ? _previousPage : null,
+                        backgroundColor:
+                            _currentPage > 0 ? Colors.blue : Colors.grey,
+                        child: const Icon(Icons.navigate_before),
+                      ),
+                      Container(
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 16,
+                          vertical: 8,
+                        ),
+                        decoration: BoxDecoration(
+                          color: Colors.black.withOpacity(0.7),
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: Text(
+                          'Page ${_currentPage + 1} of $_totalPages',
+                          style: const TextStyle(
+                            color: Colors.white,
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
+                      FloatingActionButton(
+                        onPressed:
+                            _currentPage < _totalPages - 1 ? _nextPage : null,
+                        backgroundColor: _currentPage < _totalPages - 1
+                            ? Colors.blue
+                            : Colors.grey,
+                        child: const Icon(Icons.navigate_next),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          );
+        },
       ),
-      body: Stack(children: [
-        PdfPreview(
-          build: (format) => _loadPdf(),
-        ),
-        Text(
-          'AAAAAAAAAAAA',
-          style: TextStyle(fontSize: 30, color: Colors.black),
-        ),
-      ]),
     );
   }
 }
