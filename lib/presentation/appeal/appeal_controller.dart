5e56d68cb958734ec1cf685894e4262f49cf8d23
shunkat <shunkat@icloud.com>
2024-11-03 14:20:48 +0900

    一旦server側のコメント表示を全てのwidgetに

--- lib/presentation/appeal/appeal_controller.dart
+++ lib/presentation/appeal/appeal_controller.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:tfh2024/presentation/appeal/audio/audio_appeal.dart';
-import 'package:tfh2024/presentation/appeal/gif/center_gif_appeal.dart';
 import 'package:tfh2024/presentation/appeal/gif/diagonal_gif_appeal.dart';
+import 'package:tfh2024/presentation/appeal/gif/three_gif.dart';
 import 'package:tfh2024/presentation/appeal/gif/gif_flow_appeal.dart';
 import 'package:tfh2024/presentation/appeal/gif/left_bottom_gif_appeal.dart';
+import 'package:tfh2024/presentation/appeal/gif/right_bottom_gif_appeal.dart';
 import 'package:tfh2024/presentation/appeal/text/text_flow_appeal.dart';
 
 import '../../data/comment/commentModel.dart';
@@ -55,46 +56,73 @@ class _AppealControllerState extends State<AppealController> {
 
     await _audioAppeal.initialize(widget.value);
   }
+@override
+Widget build(BuildContext context) {
+  Widget mainWidget;
 
-  @override
-  Widget build(BuildContext context) {
-    switch (widget.value) {
-      // 引数が1の場合、TextFlowAppealWidgetを表示
-      case 1:
-        return TextFlowAppealWidget(
-          texts: ['こんにちは', 'おはよう', 'こんばんは', "草", "wwwwwwww", "草", "最高"],
-          minDelay: 0,
-          maxDelay: 5,
-        );
-
-      case 2:
-        // 引数が2の場合、CenterGifAppealWidgetを表示
-        return CenterGifAppealWidget(path: 'assets/gif/happy-cat.gif');
-      case 3:
-        // 引数が3の場合、AudioAppealを表示
-        return DiagonalGifAppealWidget(path: 'assets/gif/happy-cat.gif');
-      case 4:
-        // 引数が4の場合、AudioAppealを表示
-        return LeftBottomGifAppealWidget(path: 'assets/gif/dance_man.gif');
-      case 5:
-        return GifFlowAppealWidget(path: 'assets/gif/dance.gif');
-      default:
-        // その他の数値の場合、適当なウィジェット（ここではTextウィジェット）を表示
-        return StreamBuilder<List<Comment>>(
-          stream: _repository.getCommentsForPdf(widget.pdfId),
-          builder: (context, snapshot) {
-            final comments = snapshot.data ?? [];
-            final firebaseTexts =
-                comments.map((comment) => comment.content).toList();
-            final commentIds = comments.map((comment) => comment.id).toList();
-            return TextFlowAppealWidget(
-              texts: firebaseTexts,
-              commentIds: commentIds,
-              onAnimationComplete: (commentId) =>
-                  _repository.deleteComment(widget.pdfId, commentId),
-            );
-          },
-        );
-    }
+  switch (widget.value) {
+    case 0:
+      mainWidget = TextFlowAppealWidget(
+        texts: ['こんにちは', 'おはよう', 'こんばんは', "草", "wwwwwwww", "草", "最高"],
+        minDelay: 0,
+        maxDelay: 5,
+      );
+      break;
+    case 1:
+      mainWidget = RightBottomGifAppealWidget(
+        path: 'assets/gif/kyokan.gif',
+        initialDelay: const Duration(seconds: 10),
+        displayDuration: const Duration(seconds: 10),
+      );
+      break;
+    case 2:
+      mainWidget = LeftBottomGifAppealWidget(
+        path: 'assets/gif/awsome.gif',
+        initialDelay: const Duration(seconds: 8),
+        displayDuration: const Duration(seconds: 100),
+      );
+      break;
+    case 3:
+      mainWidget = ThreeGifWidget(
+        path1: 'assets/gif/dance.gif',
+        path2: 'assets/gif/dance_women.gif',
+        path3: 'assets/gif/oji.gif',
+      );
+      break;
+    case 4:
+      mainWidget = DiagonalGifAppealWidget(
+        path: 'assets/gif/happy-cat.gif',
+      );
+      break;
+    case 5:
+      mainWidget = GifFlowAppealWidget(
+        path: 'assets/gif/dance.gif',
+      );
+      break;
+    default:
+      mainWidget = SizedBox.shrink(); // Empty widget for default case
   }
+
+  return Stack(
+    children: [
+      mainWidget,
+      // Add the StreamBuilder on top of the mainWidget
+      StreamBuilder<List<Comment>>(
+        stream: _repository.getCommentsForPdf(widget.pdfId),
+        builder: (context, snapshot) {
+          final comments = snapshot.data ?? [];
+          final firebaseTexts =
+              comments.map((comment) => comment.content).toList();
+          final commentIds = comments.map((comment) => comment.id).toList();
+          return TextFlowAppealWidget(
+            texts: firebaseTexts,
+            commentIds: commentIds,
+            onAnimationComplete: (commentId) =>
+                _repository.deleteComment(widget.pdfId, commentId),
+          );
+        },
+      ),
+    ],
+  );
+}
 }
